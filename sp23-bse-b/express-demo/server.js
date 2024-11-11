bf129d6bee20615554c61fb61cff2dbfc5c9edb5
Usman Akram <usman.akram@gmail.com>
2024-11-11 15:27:05 +0500

    b section layouts

--- sp23-bse-b/express-demo/server.js
+++ sp23-bse-b/express-demo/server.js
@@ -1,17 +1,15 @@
 const express = require("express");
+var expressLayouts = require("express-ejs-layouts");
 let server = express();
-
 server.set("view engine", "ejs");
-
+server.use(expressLayouts);
 server.use(express.static("public"));
-
 server.get("/about-me", (req, res) => {
-  res.render("about-me");
+  return res.render("about-me");
 });
 server.get("/", (req, res) => {
-  res.send(res.render("homepage"));
+  return res.send(res.render("homepage"));
 });
-
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
