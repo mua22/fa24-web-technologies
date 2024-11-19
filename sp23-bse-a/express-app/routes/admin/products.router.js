36a63054a5f7528b7f74ade8bc120c8c3b6e666a
Usman Akram <usman.akram@gmail.com>
2024-11-19 12:16:26 +0500

    section a form submission

--- sp23-bse-a/express-app/routes/admin/products.router.js
+++ sp23-bse-a/express-app/routes/admin/products.router.js
@@ -1,23 +1,23 @@
 const express = require("express");
 let router = express.Router();
+let Product = require("../../models/product.model");
 router.get("/admin/dashboard", (req, res) => {
   res.render("admin/dashboard", { layout: "admin/admin-layout" });
 });
-router.get("/admin/products", (req, res) => {
-  let products = [
-    {
-      title: "Pencil",
-      description: "This is fine pencil",
-      price: 500,
-      isFeatured: true,
-    },
-    {
-      title: "Red Pencil",
-      description: "This is fine red pencil",
-      price: 600,
-      isFeatured: true,
-    },
-  ];
+router.get("/admin/products/create", (req, res) => {
+  // if (req.query.title) return res.send(req.query);
+  // res.send("This is products creat page");
+  res.render("admin/product-form", { layout: "admin/admin-layout" });
+});
+router.post("/admin/products/create", async (req, res) => {
+  let newProduct = new Product(req.body);
+  newProduct.isFeatured = Boolean(req.body.isFeatured);
+  await newProduct.save();
+  // return res.send(newProduct);
+  return res.redirect("/admin/products");
+});
+router.get("/admin/products", async (req, res) => {
+  let products = await Product.find();
   res.render("admin/products", { layout: "admin/admin-layout", products });
 });
 module.exports = router;
--- sp23-bse-a/express-app/server.js
+++ sp23-bse-a/express-app/server.js
@@ -1,13 +1,28 @@
+// require express to make node server
+// we do not use http as its more robust
 const express = require("express");
+//call express function to make server object
 let app = express();
+// require mongoose which is ORM Object Relational Model
 const mongoose = require("mongoose");
+// require package layout options in html rendering
 var expressLayouts = require("express-ejs-layouts");
+// publically accessible assets placed in public folder are exposed
 app.use(express.static("public"));
 
+// add a middleware to parse body data for form submission
+app.use(express.urlencoded());
+
+// setup view engine. ejs must be installed
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+//add as many routers as you want
+// idea is to have seperate files for similar routes
 let productsRouter = require("./routes/admin/products.router");
 app.use(productsRouter);
+
+// add as many routes as you want like below
+//each route is distinguished according to two paramaters 1. url 2. method (http)
 app.get("/contact-us", (req, res) => {
   let address = "CUI Lahore Defence Road Off Raiwind Road";
   let phone = "+92123456";
