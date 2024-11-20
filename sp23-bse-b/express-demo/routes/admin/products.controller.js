9e1d5f8d0a80cf95043b73badcfcfb3c5b33b15c
Usman Akram <usman.akram@gmail.com>
2024-11-20 16:00:34 +0500

    section b crud

--- sp23-bse-b/express-demo/routes/admin/products.controller.js
+++ sp23-bse-b/express-demo/routes/admin/products.controller.js
@@ -1,10 +1,41 @@
 const express = require("express");
 let router = express.Router();
 let Product = require("../../models/product.model");
+
+// route to handle Delete of product
+router.get("/admin/products/delete/:id", async (req, res) => {
+  let params = req.params;
+  let product = await Product.findByIdAndDelete(req.params.id);
+  // let query = req.query;
+  // return res.send({ query, params });
+  // return res.send(product);
+  return res.redirect("/admin/products");
+});
+
+//route to render edit product form
+router.get("/admin/products/edit/:id", async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  return res.render("admin/product-edit-form", {
+    layout: "adminlayout",
+    product,
+  });
+});
+router.post("/admin/products/edit/:id", async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  product.title = req.body.title;
+  product.description = req.body.description;
+  product.price = req.body.price;
+  await product.save();
+  return res.redirect("/admin/products");
+});
+
+// route to render create product form
 router.get("/admin/products/create", (req, res) => {
   return res.render("admin/product-form", { layout: "adminlayout" });
 });
 
+//route to handle create product form submission
+// demonstrates PRG Design Pattern (Post Redirect GET)
 router.post("/admin/products/create", async (req, res) => {
   let data = req.body;
   let newProduct = new Product(data);
--- sp23-bse-b/express-demo/server.js
+++ sp23-bse-b/express-demo/server.js
@@ -1,28 +1,44 @@
+// Require Express which is light weight framework of node for web servers
 const express = require("express");
+// mongoose is ORM (Object Relational Model) for mongodb
 const mongoose = require("mongoose");
+
+// require a package for layout support
 var expressLayouts = require("express-ejs-layouts");
+// make an express server object by calling express function
 let server = express();
+// setup ejs as view engine
 server.set("view engine", "ejs");
+// add middleware for layouts
 server.use(expressLayouts);
+
+//expose public folder for publically accessible static files
 server.use(express.static("public"));
+// add support for fetching data from request body
 server.use(express.urlencoded());
 
+// make as manu routers (mini express applications) as you want and add them to express
 let adminProductsRouter = require("./routes/admin/products.controller");
 server.use(adminProductsRouter);
 
+// add as many routes as you want
 server.get("/about-me", (req, res) => {
   return res.render("about-me");
 });
-server.get("/", (req, res) => {
-  return res.send(res.render("homepage"));
+server.get("/", async (req, res) => {
+  let Product = require("./models/product.model");
+  let products = await Product.find();
+  return res.render("homepage", { products });
 });
 // let connectionString =
 //   "mongodb+srv://musmanakram:musmanakram123@cluster0.vidd3.mongodb.net/";
+// connect your mongoose by giving connection string
 let connectionString = "mongodb://localhost/sp23-bse-b";
 mongoose
   .connect(connectionString, { useNewUrlParser: true })
   .then(() => console.log("Connected to Mongo DB Server: " + connectionString))
   .catch((error) => console.log(error.message));
+// fire up the server software at port
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
